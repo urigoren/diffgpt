230aca5acc9c95e374c82b86aea3c60e7e88b3a7
Uri Goren <ugoren@argmaxml.com>
2024-09-29 16:31:21 -0400

    Readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# DiffGPT
+## Write emails like you write code
+DiffGPT is a git inspired UI to show you the edits that ChatGPT suggests.
+
+## Usage
+Draft an email, hit ctrl+enter and see how ChatGPT thinks your wording could be improved.
+
+You can also get edit suggestions for a sentence or two.
+
+You'll get a list of several possible modifications to choose from.
--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once 'config.php';
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Review my email</title>
+	<title>DiffGPT</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 	<link rel="stylesheet" href="style.css"/>
 </head>
@@ -13,14 +13,13 @@ require_once 'config.php';
 	<div class="container">
 		<div class="row">
 			<div class="col-12">
-				<h1 class="h1">Review my email</h1>
-				<p>Write your email below and click "Review" to get a few suggestions from our AI.</p>
+				<h1 class="h1">Code-Review My Email</h1>
+				<p>Write your email below and click "Suggest Edits" to get a few suggestions from our AI.</p>
 			</div>
 			<div class="col-12">
-				<textarea id="original" class="form-control" rows="10">
-Sorry Friday won't work for me, next week?
-				</textarea>
-				<button onclick="paraphrase()" class="btn btn-primary">Review (ctrl+enter)</button>
+				<textarea id="original" class="form-control" rows="10">Hi John,
+I can't make it tomorrow, I'm sick. are you available next week?</textarea>
+				<button onclick="paraphrase()" class="btn btn-primary">Suggest Edits (ctrl+enter)</button>
 				<button onclick="copy()" class="btn btn-info">Copy</button>
 				<button onclick="reset()" class="btn btn-secondary">Reset</button>
 			</div>
